512f3b327ad36187342034854595451e2d7b385e
Kimathi Devis <kimathidevis@gmail.com>
2026-07-05 11:49:25 +0300

    Add customer portal account model

--- portal/models.py
+++ portal/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+from clients.models import Client
+
+
+class PortalAccount(models.Model):
+
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name="portal_account"
+    )
+
+    client = models.OneToOneField(
+        Client,
+        on_delete=models.CASCADE,
+        related_name="portal_account"
+    )
+
+    is_verified = models.BooleanField(
+        default=False
+    )
+
+    activated_at = models.DateTimeField(
+        null=True,
+        blank=True
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    updated_at = models.DateTimeField(
+        auto_now=True
+    )
+
+
+    def __str__(self):
+
+        return (
+            f"{self.client.name} Portal Account"
+        )
